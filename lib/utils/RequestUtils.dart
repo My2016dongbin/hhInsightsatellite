b806cb844836893e9a56b8829eef318b156a6133
dongbin <qc.dancer@qq.com>
2025-05-19 10:20:28 +0800

    慧眼卫星3.0 -多区域选择问题修复 -map circle清除 -release

--- lib/utils/RequestUtils.dart
+++ lib/utils/RequestUtils.dart
@@ -1,9 +1,9 @@
 
 class RequestUtils{
-  static const base = 'http://192.168.1.88:51158';//app
+  // static const base = 'http://192.168.1.88:51158';//app
   // static const base = 'http://192.168.1.88:9528/api';//web
   // static const base = 'http://192.168.1.88:8011/api';//out
-  // static const base = 'http://192.168.1.103:9900';//debug
+  static const base = 'http://192.168.1.103:9900';//release
 
   static const login = '$base/auth/login';//密码登录-
   static const userInfo = '$base/system/user/getInfo/app';//个人信息查询-
