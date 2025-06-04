c29c0895eeecdb7dc8130c7e359c3c54744e6e0c
dongbin <qc.dancer@qq.com>
2025-06-04 11:27:17 +0800

    慧眼卫星3.0 -域名外网

--- lib/utils/RequestUtils.dart
+++ lib/utils/RequestUtils.dart
@@ -3,7 +3,8 @@ class RequestUtils{
   // static const base = 'http://192.168.1.88:51158';//app
   // static const base = 'http://192.168.1.88:9528/api';//web
   // static const base = 'http://192.168.1.88:8011/api';//out
-  static const base = 'http://192.168.1.102:9900';//release
+  // static const base = 'http://192.168.1.102:9900';//release
+  static const base = 'http://wechattestfront.ehaohai.com/api';//域名外网
 
   static const login = '$base/auth/login';//密码登录-
   static const userInfo = '$base/system/user/getInfo/app';//个人信息查询-
